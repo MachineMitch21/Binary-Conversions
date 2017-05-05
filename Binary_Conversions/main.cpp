7dfe145cbfbbc63bbc4133cbe34ce86df3dacb9f
MachineMitch21 <mitchschutt@outlook.com>
2017-05-05 13:38:55 -0400

    Added some formatting to program output

--- Binary_Conversions/main.cpp
+++ Binary_Conversions/main.cpp
@@ -6,14 +6,20 @@
 
 int main(int argv, char *argc[]){
 
+	std::endl;
+	std::cout << "Binary Conversion program BINNY" << std::endl;
+	std::cout << "Written by: Mitch Schutt" << std::endl;
+	std::endl;
+	std::endl;
+
 	BinConverter bin_con;
 	std::string str(argc[2]);
 
 	if(argc[1][0] == 'i' || argc[1][0] == 'c')
-		std::cout << bin_con.getBin(str, argc[1][0]) << std::endl;
+		std::cout << "Output: " << bin_con.getBin(str, argc[1][0]) << std::endl;
 
 	else if(strcmp(argc[1], DECIMAL) != 0 || strcmp(argc[1], ASCII) != 0)
-		std::cout << bin_con.binStr(str, argc[1]) << std::endl;
+		std::cout << "Output: " << bin_con.binStr(str, argc[1]) << std::endl;
 }
 
 
